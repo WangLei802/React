815cd9cbd3968b8e1b4b634b8092ad31b4d9d347
DESKTOP-5DF6JO8\MSI <wl18735189236@163.com>
2019-08-14 21:34:39 +0800

    4

--- REdemo/src/testComponent/testComponent.jsx
+++ REdemo/src/testComponent/testComponent.jsx
@@ -12,7 +12,7 @@ class testComponent extends Component{
             <Fragment>
                <div><input /> <button> 增加服务 </button></div>
                <ul>
-                   <li>头部按摩</li>
+                   <li>头部按摩4</li>
                    <li>精油推背</li>
                </ul> 
             </Fragment>
