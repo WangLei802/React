10128fee2fc91020f844448fe5649b94dead2419
DESKTOP-RSUEQJK\冯世奇 <wl18735189236@163.com>
2019-09-26 19:17:40 +0800

    vue+express

---
+++ Vue/demo/Express/vueproject/src/main.js
@@ -0,0 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import {post,get} from './request/http'
+
+Vue.prototype.$post = post;
+Vue.prototype.$get = get;
+Vue.config.productionTip = false
+// var url = process.env.BASE_API
+// axios.get(url+'/product')
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+  console.log(process.env)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
