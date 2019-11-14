2949164375c268bbc28152fec62d3d8879baac87
wangl <wanglei@shxrtech.com>
2019-11-14 16:08:40 +0800

    Redux-thunk  测试

--- React/react+antd/antd-demo/src/store/actionCreators.js
+++ React/react+antd/antd-demo/src/store/actionCreators.js
@@ -2,7 +2,7 @@ import  {GET_LIST,CHANGE_INPUT,ADD_ITEM,DEL}  from './actionTypes'
 
 export const getTodoList = () =>{
     return (dispatch)=>{
-        const data = [1,2]
+        const data = [1,3]
         const action = getList(data)
         dispatch(action)
     }
